79cd0fcd2ee53089661e5cc6298b5a6ec518fcb0
Freiii21 <mrfreiii@gmail.com>
2022-03-06 15:40:22 +0300

    Remove temp reducer

--- src/n1-main/m2-bll/store.ts
+++ src/n1-main/m2-bll/store.ts
@@ -1,12 +1,10 @@
 import {combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
-import { Reducer } from "./reducer";
 import {authReducer} from './authReducer';
 import {testReducer} from './testReducer';
 
 
 const rootReducer = combineReducers({
-    temp: Reducer,
     auth:authReducer,
     test:testReducer,
 })
