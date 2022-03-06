7ed79e26417901aef7ac87a6f2a34845fc3c306d
Kirill <3331668@mail.ru>
2022-03-06 10:13:02 +0300

    create profile page

--- src/n2-features/f1-auth/a3-profile/Profile.tsx
+++ src/n2-features/f1-auth/a3-profile/Profile.tsx
@@ -1,7 +1,32 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import SuperButton from "../../../n1-main/m1-ui/common/c2-SuperButton/SuperButton";
+import {setLogoutT} from "../../../n1-main/m2-bll/reducer";
+import {useTypedSelector} from "../../../n1-main/m2-bll/redux"
+import { DateTime } from 'luxon'
+
+import s from './Profile.module.css'
+
+
+
 export const Profile = () => {
+    const dispatch = useDispatch();
+    const handleLogout = () => dispatch(setLogoutT());
+
+    const user = useTypedSelector(state => state.auth)
+
+    const registerData = DateTime.fromISO(user.created).toFormat('DDD')
+
     return (
-        <div>
-            Profile page
+        <div className={s.wrapper}>
+            {user.avatar ? <img src={user.avatar} alt="AVATAR"/> : <div className={'avatar'}>{user.name.substring(0, 2)}</div>}
+
+            <span>Your Email: {user.email}</span>
+            <span>Your NickName: {user.email}</span>
+            <span>Date of registration: {registerData}</span>
+            <div>
+                <SuperButton onClick={handleLogout}>Logout</SuperButton>
+            </div>
         </div>
     )
 }
