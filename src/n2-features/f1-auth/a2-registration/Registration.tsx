ddb5742eb8b9e517c02565fec9906a1a4a017419
Freiii21 <mrfreiii@gmail.com>
2022-03-06 17:30:30 +0300

    created UI elements

--- src/n2-features/f1-auth/a2-registration/Registration.tsx
+++ src/n2-features/f1-auth/a2-registration/Registration.tsx
@@ -1,7 +1,28 @@
+import s from './Registration.module.css'
+
 export const Registration = () => {
     return (
-        <div>
-            Registration page
+        <div className={s.common}>
+            <div className={s.regWindow}>
+                <div className={s.text}>
+                    <span>It-incubator</span>
+                </div>
+                <div className={s.text2}>
+                    <span>Sign Up</span>
+                </div>
+                <div>
+                    <input type="email" placeholder="Enter email"/>
+                </div>
+                <div>
+                    <input type="password" placeholder="Enter password"/>
+                </div>
+                <div>
+                    <input type="password" placeholder="Confirm password"/>
+                </div>
+                <div>
+                    <button>Register</button>
+                </div>
+            </div>
         </div>
     )
 }
