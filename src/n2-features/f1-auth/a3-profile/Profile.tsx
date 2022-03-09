997d04c1da5a5b6b0f61e2c578cff8e9721615cc
Lelik Jan <21alex7777@gmail.com>
2022-03-09 20:14:15 +0300

    from lelik: delete button log out from Profile.tsx

--- src/n2-features/f1-auth/a3-profile/Profile.tsx
+++ src/n2-features/f1-auth/a3-profile/Profile.tsx
@@ -14,8 +14,6 @@ import s from './Profile.module.css'
 
 export const Profile = () => {
     const dispatch = useDispatch();
-    const handleLogout = () => dispatch(setLogoutT());
-
     const user = useTypedSelector(state => state.auth.user)
 
     const registerData = DateTime.fromISO(user.created).toFormat('DDD')
@@ -34,9 +32,7 @@ export const Profile = () => {
             <span>Your Email: {user.email}</span>
             <span>Your NickName: {user.email}</span>
             <span>Date of registration: {registerData}</span>
-            <div>
-                <SuperButton onClick={handleLogout}>Logout</SuperButton>
-            </div>
+
         </div>
     )
 }
