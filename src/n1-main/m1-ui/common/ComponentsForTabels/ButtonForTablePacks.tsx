97d72a7bd8b5de817d9aa24e0c192b657d3428e3
Lelik Jan <21alex7777@gmail.com>
2022-04-04 20:32:28 +0300

    from lelik: change appearance of arrows when click on name cell to sort column

--- src/n1-main/m1-ui/common/ComponentsForTabels/ButtonForTablePacks.tsx
+++ src/n1-main/m1-ui/common/ComponentsForTabels/ButtonForTablePacks.tsx
@@ -8,38 +8,52 @@ import {useTypedSelector} from '../../../m2-bll/redux';
 type PropsType = {
     nameCell: string;
     handlerSetSortPacs?: (sortValue: string) => void;
+    setNameCellOnClick: (n: string) => void;
+    nameCellOnClick: string;
 }
-export const ButtonForTablePacks = ({nameCell, handlerSetSortPacs}: PropsType) => {
-    const [arrow, setArrow] = useState(true);
-    const status = useTypedSelector(state => state.app.status);
-    let onClickHandler = () => {
-        if (arrow) {
-            handlerSetSortPacs && handlerSetSortPacs(`0${nameCell}`);
-            setArrow(!arrow);
-        } else {
-            handlerSetSortPacs && handlerSetSortPacs(`1${nameCell}`);
-            setArrow(!arrow);
-        }
+export const ButtonForTablePacks: React.FC<PropsType> =
+    ({
+         nameCell, handlerSetSortPacs,
+         setNameCellOnClick, nameCellOnClick,
+         children
+     }) => {
+        const [arrow, setArrow] = useState(true);
+        const status = useTypedSelector(state => state.app.status);
+        let onClickHandler = () => {
+            setNameCellOnClick && setNameCellOnClick(nameCell);
 
-    };
-    return (
-        <button
-            style={{
-                width: '27px',
-                color: colorBlueMI,
-                border: 'none',
-                position: 'relative',
-                top: '5px',
-                cursor: 'pointer',
-            }}
-            disabled={status === 'loading'}
-            onClick={onClickHandler}
-        >
-            {
-                arrow ? <ArrowDownwardIcon/> : <ArrowUpwardIcon/>
+            if (arrow) {
+                handlerSetSortPacs && handlerSetSortPacs(`0${nameCell}`);
+                setArrow(!arrow);
+            } else {
+                handlerSetSortPacs && handlerSetSortPacs(`1${nameCell}`);
+                setArrow(!arrow);
             }
 
 
-        </button>
-    )
-}
+        };
+        console.log('ButtonForTablePacks')
+        return (
+            <button
+                style={{
+                    width: '27px',
+                    color: colorBlueMI,
+                    border: 'none',
+                    position: 'relative',
+                    top: '5px',
+                    cursor: 'pointer',
+                    display: 'flex',
+                    flexDirection: 'row',
+                }}
+                disabled={status === 'loading'}
+                onClick={onClickHandler}
+            >
+                <span>{children}</span>
+                {
+                    !(nameCellOnClick === nameCell) ? '' : arrow ? <ArrowDownwardIcon/> : <ArrowUpwardIcon/>
+                }
+
+
+            </button>
+        )
+    }
